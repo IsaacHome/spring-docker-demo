d2213353a7d88597ee0baae6680e95c6421e3ecc
isaachome <g.isaacvhm@gmail.com>
2019-07-14 09:42:34 +0630

    first commit

---
+++ spring-docker/src/main/java/com/solt/springdocker/controller/EmployeeController.java
@@ -0,0 +1,39 @@
+package com.solt.springdocker.controller;
+
+import com.solt.springdocker.model.Employee;
+import com.solt.springdocker.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public String  createEmployee(){
+        Arrays.asList(new Employee("Thaw Thaw","HR",300000),
+                new Employee("Thaw Thaw","HR",200000),
+                new Employee("Thaw Thaw","HR",100000)
+                )
+        .forEach(employeeRepository::save);
+
+
+        return "Successfully Create";
+
+
+
+    }
+
+    @GetMapping("/all")
+    public List<Employee> findAllEmployee(){
+        return employeeRepository.findAll();
+    }
+}
---
+++ spring-docker/src/main/java/com/solt/springdocker/model/Employee.java
@@ -0,0 +1,27 @@
+package com.solt.springdocker.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String department;
+    private int salary;
+
+
+    public Employee(String name, String department, int salary) {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+}
